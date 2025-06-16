43248669616b1a164bfd2033a354ac2686701f2e
MartinMarrerooo <martin.marrero@estudiantes.utec.edu.uy>
2025-06-16 00:37:50 -0300

    un poco de todo

---
+++ cmake-build-debug/CMakeFiles/Dis_EjemploArquitecturaLogin.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Laboratorio4pa/src/DTs/DTCliente.cpp" "CMakeFiles/Dis_EjemploArquitecturaLogin.dir/src/DTs/DTCliente.cpp.obj" "gcc" "CMakeFiles/Dis_EjemploArquitecturaLogin.dir/src/DTs/DTCliente.cpp.obj.d"
+  "C:/Laboratorio4pa/src/DTs/DTDireccion.cpp" "CMakeFiles/Dis_EjemploArquitecturaLogin.dir/src/DTs/DTDireccion.cpp.obj" "gcc" "CMakeFiles/Dis_EjemploArquitecturaLogin.dir/src/DTs/DTDireccion.cpp.obj.d"
+  "C:/Laboratorio4pa/src/DTs/DTFecha.cpp" "CMakeFiles/Dis_EjemploArquitecturaLogin.dir/src/DTs/DTFecha.cpp.obj" "gcc" "CMakeFiles/Dis_EjemploArquitecturaLogin.dir/src/DTs/DTFecha.cpp.obj.d"
+  "C:/Laboratorio4pa/src/DTs/DTUsuario.cpp" "CMakeFiles/Dis_EjemploArquitecturaLogin.dir/src/DTs/DTUsuario.cpp.obj" "gcc" "CMakeFiles/Dis_EjemploArquitecturaLogin.dir/src/DTs/DTUsuario.cpp.obj.d"
+  "C:/Laboratorio4pa/src/DTs/DTVendedor.cpp" "CMakeFiles/Dis_EjemploArquitecturaLogin.dir/src/DTs/DTVendedor.cpp.obj" "gcc" "CMakeFiles/Dis_EjemploArquitecturaLogin.dir/src/DTs/DTVendedor.cpp.obj.d"
+  "C:/Laboratorio4pa/src/Principal.cpp" "CMakeFiles/Dis_EjemploArquitecturaLogin.dir/src/Principal.cpp.obj" "gcc" "CMakeFiles/Dis_EjemploArquitecturaLogin.dir/src/Principal.cpp.obj.d"
+  "C:/Laboratorio4pa/src/logica/controlador/SistemaControlador.cpp" "CMakeFiles/Dis_EjemploArquitecturaLogin.dir/src/logica/controlador/SistemaControlador.cpp.obj" "gcc" "CMakeFiles/Dis_EjemploArquitecturaLogin.dir/src/logica/controlador/SistemaControlador.cpp.obj.d"
+  "C:/Laboratorio4pa/src/logica/dominio/Usuario.cpp" "CMakeFiles/Dis_EjemploArquitecturaLogin.dir/src/logica/dominio/Usuario.cpp.obj" "gcc" "CMakeFiles/Dis_EjemploArquitecturaLogin.dir/src/logica/dominio/Usuario.cpp.obj.d"
+  "C:/Laboratorio4pa/src/presentacion/AltaUsuario.cpp" "CMakeFiles/Dis_EjemploArquitecturaLogin.dir/src/presentacion/AltaUsuario.cpp.obj" "gcc" "CMakeFiles/Dis_EjemploArquitecturaLogin.dir/src/presentacion/AltaUsuario.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Dis_EjemploArquitecturaLogin.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Dis_EjemploArquitecturaLogin.
+# This may be replaced when dependencies are built.
