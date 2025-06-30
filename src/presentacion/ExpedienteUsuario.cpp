ee11b5bedef5a1dc5d183031d7e2fb853d9ca1a4
Agustin Machado <agusm.s.2006@gmail.com>
2025-06-29 22:55:18 -0300

    probando

---
+++ src/presentacion/ExpedienteUsuario.cpp
@@ -0,0 +1,5 @@
+//
+// Created by marti on 29/6/2025.
+//
+
+#include "ExpedienteUsuario.h"
---
+++ src/presentacion/ExpedienteUsuario.h
@@ -0,0 +1,16 @@
+//
+// Created by marti on 29/6/2025.
+//
+
+#ifndef EXPEDIENTEUSUARIO_H
+#define EXPEDIENTEUSUARIO_H
+
+
+
+class ExpedienteUsuario {
+
+};
+
+
+
+#endif //EXPEDIENTEUSUARIO_H
